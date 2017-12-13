97e300003b618a1e085c4c1711656e8cf598bd92
takashabe <takashabe.public@gmail.com>
2017-12-13 09:46:00 +0900

    Add testcase

--- interfaces/handler_test.go
+++ interfaces/handler_test.go
@@ -1,7 +1,9 @@
 package interfaces
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,6 +12,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/takashabe/go-ddd-sample/domain"
 	fixture "github.com/takashabe/go-fixture"
 )
 
@@ -113,6 +116,58 @@ func TestGetUsers(t *testing.T) {
 		}
 		if !reflect.DeepEqual(c.expectJSON, payload) {
 			t.Errorf("#%d: want %s, got %s", i, c.expectJSON, payload)
+		}
+	}
+}
+
+func TestCreateUsers(t *testing.T) {
+	ts := prepareServer(t)
+	defer ts.Close()
+
+	cases := []struct {
+		input      string
+		expectCode int
+	}{
+		{"foo", http.StatusCreated},
+	}
+	for i, c := range cases {
+		// POST "/user"
+		type post struct {
+			Name string `json:"name"`
+		}
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(post{Name: c.input})
+		if err != nil {
+			t.Fatalf("#%d: want non error, got %#v", i, err)
+		}
+		url := fmt.Sprintf("%s/user", ts.URL)
+		res := sendRequest(t, "POST", url, &buf)
+		defer res.Body.Close()
+		if c.expectCode != res.StatusCode {
+			t.Errorf("#%d: want %d, got %d", i, c.expectCode, res.StatusCode)
+		}
+
+		// GET "/users"
+		url = fmt.Sprintf("%s/users", ts.URL)
+		res = sendRequest(t, "GET", url, nil)
+		defer res.Body.Close()
+		type get struct {
+			Users []*domain.User `json:"users"`
+		}
+		var receive get
+		err = json.NewDecoder(res.Body).Decode(&receive)
+		if err != nil {
+			t.Fatalf("#%d: want non error, got %#v", i, err)
+		}
+		find := false
+		for _, u := range receive.Users {
+			if u.Name == c.input {
+				find = true
+				break
+			}
+		}
+		if !find {
+			t.Errorf("#%d: not found created user %q", i, c.input)
 		}
 	}
 }
